12c84bdbcb28c1ff436097edb389616f17c3550d
SaiJyothi Gudibandi <saijyothigudibandi9@gmail.com>
2020-04-02 16:21:13 -0700

    Pushing files

---
+++ job-dsl/demo.groovy
@@ -0,0 +1,15 @@
+pipelineJob('DSL_Demo') {
+
+    def repo = 'https://github.com/SaiJyothiGudibandi/sample-project-maven.git'
+    triggers {
+        scm('*/15 * * * *')
+    }
+    
+    definition {
+        cpsScm {
+          scm {
+            git(repo, 'master', { node -> node / 'extensions' << '' } )
+            }
+        }
+    }
+}
