bbd7baf5421c805cccd8a57aaec19e66ef76535f
SaiJyothi Gudibandi <saijyothigudibandi9@gmail.com>
2020-04-03 12:00:24 -0700

    Changing demo.g

--- job-dsl/demo.groovy
+++ job-dsl/demo.groovy
@@ -1,4 +1,7 @@
-pipelineJob('Test-DSL-parameterized-Child-Pipeline-'+${env.BUILD_NUMBER}) {
+pipelineJob('Test-DSL-parameterized-Child-Pipeline') {
+    wrappers {
+        buildName('#${BUILD_NUMBER} on ${ENV,var="BRANCH"}')
+    }
     parameters {
         stringParam('testVarJob', 'my default stringParam value', 'my description')
     }
