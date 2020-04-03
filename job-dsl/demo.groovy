000009e74ae50a3dd406163beaedaf29f7c11ec2
SaiJyothiGudibandi <saijyothigudibandi9@gmail.com>
2020-04-02 18:18:26 -0700

    Update demo.groovy

--- job-dsl/demo.groovy
+++ job-dsl/demo.groovy
@@ -1,6 +1,6 @@
 pipelineJob('Test-DSL-parameterized-Jenkins') {
     parameters {
-        string(defaultValue: "12345", description: 'Testing variable', name: 'testvar1')
+        string(defaultValue: "Testing", description: 'Testing variable', name: 'testvar1')
     }
 
     def repo = 'https://github.com/SaiJyothiGudibandi/Test-sample-project-maven.git'
