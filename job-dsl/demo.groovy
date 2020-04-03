ff2ecb5b0741ef970a919fd07f0846d1b90d54d0
SaiJyothi Gudibandi <saijyothigudibandi9@gmail.com>
2020-04-02 18:42:04 -0700

    edited

--- job-dsl/demo.groovy
+++ job-dsl/demo.groovy
@@ -1,7 +1,7 @@
 pipelineJob('Test-DSL-parameterized-Child-Pipeline') {
 
     parameters {
-        stringParam( "testVarJob", "${params.$testvar}" )
+        stringParam( "testVarJob", $testvar )
     }
 
     def repo = 'https://github.com/SaiJyothiGudibandi/Test-sample-project-maven.git'
